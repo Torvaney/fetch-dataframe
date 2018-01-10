17c8bad7b7ee65f8624fd27344ee7a9ddac16f1c
Torvaney <btorvaney@gmail.com>
2018-01-10 19:04:58 +0000

    Update config sample

--- config.sample.py
+++ config.sample.py
@@ -1,8 +1,12 @@
 """ Database connections. """
 
-DB = dict(
-    host='',
-    user='',
-    password='',
-    database=''
-)
+DB = {
+    'mydatabase': {
+        'host': 'localhost',
+        'username': 'me',
+        'password': '4g00dpa55word',
+        'database': 'db',
+        'port': '5432',
+        'dialect': 'postgresql'
+    },
+}
