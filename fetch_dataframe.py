b998feac383d27f29093c69542ca3203905e7e45
Torvaney <btorvaney@gmail.com>
2018-01-27 16:03:24 +0000

    Allow query parameters

--- fetch_dataframe.py
+++ fetch_dataframe.py
@@ -18,7 +18,7 @@ def fetch_dataframe(sql_path, config_name, params=None):
     with open(sql_path, "r") as query_file:
         connection = get_db_connection(config_name)
         query = query_file.read()
-        if params is not None:
+        if params:
             for key, value in params.items():
                 query = query.replace("@" + key, str(value))
     return pd.read_sql(query, con=connection)
@@ -30,10 +30,17 @@ if __name__ == '__main__':
     parser.add_argument('datapath', type=str, help='Path to output data.')
     parser.add_argument('--config', default='mendieta', type=str,
                         help='Which config to use.')
+    parser.add_argument('--params', nargs='*', type=str,
+                        help='Query parameters as keyword:argument pairs')
     parser.add_argument('--print', action='store_true', help='Print a preview.')
     args = parser.parse_args()
 
-    data = fetch_dataframe(args.sqlpath, args.config)
+    params = {}
+    for param in args.params:
+        kw, arg = param.split(':')
+        params[kw] = arg
+
+    data = fetch_dataframe(args.sqlpath, args.config, params=params)
 
     if args.print:
         print(data.head())
